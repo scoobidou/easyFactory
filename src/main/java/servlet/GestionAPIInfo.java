3c226dba658a3ddce751deb0cb915c240240762e
Manu <Manu@Manu-PC.home>
2016-12-07 02:53:22 +0100

    helloWorld - j'ai rajouté quelques informatiosn pour l'api docker mais aussi le support bootstrap

--- src/main/java/servlet/GestionAPIInfo.java
+++ src/main/java/servlet/GestionAPIInfo.java
@@ -2,14 +2,19 @@ package servlet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 public class GestionAPIInfo extends HttpServlet {
 	
@@ -26,19 +31,23 @@ public class GestionAPIInfo extends HttpServlet {
 		
 		requestUrl.connect();
 		
-//		JsonParser jsp = new JsonParser();
-//		JsonElement jse = jsp.parse(new InputStreamReader((InputStream) requestUrl.getContent()));
-//		JsonObject jso = jse.getAsJsonObject();			
-//		String azertyWin = jso.get("RepoDigests").getAsString();
+//		InputStream inputStream = (InputStream)requestUrl.getContent();
+//		
+//		Scanner s = new Scanner(inputStream).useDelimiter("\\A");
+//		String azertyWin = s.hasNext() ? s.next() : "";	
 		
-		InputStream inputStream = (InputStream)requestUrl.getContent();
+		JsonParser parser = new JsonParser();
+		JsonElement jse = parser.parse(new InputStreamReader((InputStream) requestUrl.getContent()));
+		JsonObject jso = jse.getAsJsonObject();	
 		
-		Scanner s = new Scanner(inputStream).useDelimiter("\\A");
-		String azertyWin = s.hasNext() ? s.next() : "";		
+		String azertyWin = jso.toString();
+		String blabla = jso.get("Containers").getAsString();
 		
+//		s.close();	
 		
 		request.setAttribute(ATT_MESSAGES, azertyWin);
-
+		request.setAttribute("info",blabla);
+		
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 		
 	}
--- src/main/webapp/WEB-INF/views/api_info.jsp
+++ src/main/webapp/WEB-INF/views/api_info.jsp
@@ -7,17 +7,25 @@
         <link rel="stylesheet" href="webjars/bootstrap/3.3.5/css/bootstrap.css">
     </head>
     <div class="jumbotron">
-    	<p>HELLO WORLD</p>
+    	<p>DOCKER API - INFORMATIONS</p>
     </div>
     <body>
-        <p>Ceci est une page générée depuis une JSP.</p>
+        <p>Voici les informations extraites en live du serveur Docker</p>
         <p>
+        	TOUTES LES INFORMATIONS DU /info de l'api <br/>
             <% 
             String attribut = (String) request.getAttribute("messages");
             out.println( attribut );
 
             %>
         </p>
+        <p>
+        	Le nombre de Containers : 
+        	<%
+        		String element = (String) request.getAttribute("info");
+        		out.println(element);
+        	%>
+        	<br/>
     	<script src="webjars/jquery/2.1.4/jquery.js"></script>
   		<script src="webjars/bootstrap/3.3.5/js/bootstrap.js"></script>
     </body>
