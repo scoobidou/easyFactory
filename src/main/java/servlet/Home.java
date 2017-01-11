62f77f900ef682a52e8ba93a8f4fcee56d8e3f4e
Manu <Manu@Manu-PC>
2017-01-11 19:37:13 +0100

    Creation tuto Docker & Docker Compose

--- src/main/java/servlet/Home.java
+++ src/main/java/servlet/Home.java
@@ -3,9 +3,12 @@ package servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("")
 
 public class Home extends HttpServlet {
 
--- src/main/java/servlet/ProjectPage.java
+++ src/main/java/servlet/ProjectPage.java
@@ -3,9 +3,12 @@ package servlet;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/project")
 
 public class ProjectPage extends HttpServlet {
 	
